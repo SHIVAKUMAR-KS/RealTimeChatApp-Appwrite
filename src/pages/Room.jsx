7d9eff3da584358b8a08bc5adf973e3d73c0ab97
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-09-03 12:46:55 +0530

    done with 30min

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -2,23 +2,39 @@ import React, { useState, useEffect } from 'react';
 import { COLLECTION_ID_MESSAGES, DATABASE_ID, databases } from '../appwriteConfig';
 
 const Room = () => {
+
+    const [messages, setMessages] = useState([]);
+
+
     useEffect(() => {
         getMessages();
     }, []);
 
     const getMessages = async () => {
-        try {
-            const response = await databases.listDocuments(DATABASE_ID, COLLECTION_ID_MESSAGES);
-            console.log('Response:', response);
-        } catch (error) {
-            console.error('Error fetching messages:', error);
-        }
+       
+        const response = await databases.listDocuments(DATABASE_ID, COLLECTION_ID_MESSAGES);
+        console.log('Response:', response);
+        setMessages(response.documents);
+        
     };
 
     return (
-        <div>
-            Room
-        </div>
+        <main className='container'>
+            <div className='room--container'>
+                <div>
+                    {messages.map(messages => (
+                        <div key={messages.$id}>
+                            <div>
+                                <p>{messages.$createdAt}</p>
+                            </div>
+                            <div>
+                                <span>{messages.body}</span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </main>
     );
 };
 
