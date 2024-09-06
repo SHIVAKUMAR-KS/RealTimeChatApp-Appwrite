c21f74321e05beda4219438f94444cad9ace007e
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-09-06 21:00:16 +0530

    now project is in working mode

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,5 +1,6 @@
-import { Client,Databases} from 'appwrite';
+import { Client,Databases,Account} from 'appwrite';
 
+export const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT
 export const PROJECT_ID ='66d59185001fdf41e68b'
 export const DATABASE_ID = '66d592ef00081fbccaed'
 export const COLLECTION_ID_MESSAGES = '66d59306001d3d23a982'
@@ -12,5 +13,6 @@ client
     .setProject('66d59185001fdf41e68b');
 
 
+export const account = new Account(client);
 export const databases=new Databases(client);
 export default client;
