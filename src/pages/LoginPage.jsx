084ba8eea5b6d1f7aff56f4bd329af375433c50d
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-09-10 23:22:43 +0530

    chenges

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import { useAuth } from '../utils/AuthContext'
 import { useNavigate } from 'react-router'
 import { Link } from 'react-router-dom'
-import { toast } from 'react-toastify'; // Import toast
+import { toast } from 'react-toastify'; 
 import '../index.css'
 
 
